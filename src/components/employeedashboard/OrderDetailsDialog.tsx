3bff2400c0f37a518d026d26cab9f702b991b14a
jyush98 <55372314+jyush98@users.noreply.github.com>
2024-11-26 12:54:43 -0500

    Added more order details to dashboard

--- src/components/employeedashboard/OrderDetailsDialog.tsx
+++ src/components/employeedashboard/OrderDetailsDialog.tsx
@@ -21,21 +21,22 @@ const OrderDetailsDialog: React.FC<OrderDetailsDialogProps> = ({ order, open, on
             <DialogTitle>Order Details</DialogTitle>
             <DialogContent>
                 <h3>Order Summary:</h3>
+                <h3>Time Created: {new Date(order.createdAt * 1000).toLocaleString()}</h3>
                 {order.items.map((orderItem, index) => {
                     const pizza = orderItem.pizza;
                     //console.log(order.items);
                     if (!pizza) return null;
-                    
+
                     return (
                         <div key={orderItem.id || index} style={{ marginBottom: '1rem' }}>
                             {/* Would have liked to include the pizza names or IDs here but 
                             the OrderResponse and the Pizza do not contain that info */}
-                            {/* <p>Pizza: {pizza.name ?? 'Unknown'}</p> */}
+                            <p>Pizza: {pizza.type ?? 'Unknown'}</p>
                             <p>Size: {pizza.size}</p>
-                        <p>Quantity: {pizza.quantity}</p>
-                        <p>Toppings: {(pizza.topping ?? []).map((t) => `${t.name} (${t.quantity})`).join(', ') || 'None'}</p>
-                        <p>Topping Exclusions: {pizza.toppingExclusions?.join(', ') || 'None'}</p>
-                        <p>Price per Unit: ${pizza.totalPrice.toFixed(2)}</p>
+                            <p>Quantity: {pizza.quantity}</p>
+                            <p>Toppings: {(pizza.topping ?? []).map((t) => `${t.name} (${t.quantity})`).join(', ') || 'None'}</p>
+                            <p>Topping Exclusions: {pizza.toppingExclusions?.join(', ') || 'None'}</p>
+                            <p>Price per Unit: ${pizza.totalPrice.toFixed(2)}</p>
                             <Divider style={{ margin: '1rem 0' }} />
                         </div>
                     );
